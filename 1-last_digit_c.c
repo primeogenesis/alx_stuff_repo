7a0f69f0ab43bce67fa0fa74bdbdec1717f2750d
primeogenesis <primemailin@gmail.com>
2022-10-26 00:51:19 +0100

    done

--- 1-last_digit_c.c
+++ 1-last_digit_c.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+//#include <string.h>
 #define ARRAYSIZE 65
 #define NONNUM 'g'
 
@@ -13,17 +14,40 @@ int main(void)
 {
 	int n;
 	char str[ARRAYSIZE];
+	long int lastdigit;
 	int i = 0;
+	int j;
+	int temp = 0;
 
 	srand(time(0));
 
-	for (i = 0; i < ARRAYSIZE; i++)
-		printf("%d ", i);//str[i] = NONNUM;
-
-	sprintf(str, "%d", n);
-
 	n = rand() - RAND_MAX / 2;
+	temp = n;
 
 
+	for(j = 0; j < 100; j++)
+	{
+
+		//srand(time(0));
+		n = rand() - RAND_MAX / 2;
+
+		if(temp == n && j > 0)//dg
+		{//dg
+			j--;//dg
+			continue;//dg
+		}else{ temp = n; }//dg
+
+		sprintf(str, "%d", n);
+
+		while(str[i])
+		{
+			i++;
+		}
+		i--;
+		lastdigit = strtol(str + i, NULL, 10);
+		printf("lastdigit of %d is: %ld\n", n, lastdigit);
+
+	}
 	return (0);
+
 }
