22fd44a4eea63a7331b6dd79e6b4fb1199b7cdd4
primeogenesis <primemailin@gmail.com>
2022-10-26 07:05:49 +0100

    done - a-z then A-Z

--- alx_basic_c/main.c
+++ alx_basic_c/main.c
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 
 /**
-* main - prints a-z and then newline
-* Description: did it with 1 putchar(),
-* 	gotta sacrifice simplicity though.
+* main - prints 'a'-'z' then 'A' - 'Z' and then newline
+*
 * Return: 0 (Always)
 */
 
@@ -14,21 +13,20 @@ int main()
 
 	/* ascii char 'a' is 97 which follow serially to char 'z' which 122 */
 
-	for(i = 'a'; i < 'z' + 2; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		if(i > 'z')
-		{
-			i = '\n';
-		}
 		putchar(i);
-		if(i  == '\n')
-		{
-			break;
-		}
 	}
 
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
 	return (0);
-	/*printf(" >%d to %d diff %d\n >%d to %d diff %d\n"
-			, 'a', 'z', 'a' - 'z', 'A', 'Z', 'A' - 'Z'); // bg
-	*/
+/*	printf(" >%d to %d diff %d\n >%d to %d diff %d\n"
+*	, 'a', 'z', 'a' - 'z', 'A', 'Z', 'A' - 'Z'); // bg
+*/
+
 }
